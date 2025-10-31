7bd8eab7c5ed2588752aceb1e1eb4b67ddbf5ccc
Ye Bhone Lin <yebhonelin@Yes-MacBook-Air.local>
2025-10-31 21:33:41 +0630

    debug

--- chatbot/api_endpoint/org_main.py
+++ chatbot/api_endpoint/org_main.py
@@ -1,27 +1,26 @@
-from utils import contextual_embed, chat_model_work
+from utils import chat_model_work
+from db_access import RetrieveData
 from fastapi import FastAPI
 import uvicorn 
 from contextlib import asynccontextmanager
 from schema import textRequest
-import os
 import mlflow 
+import os
 import traceback 
 
-api_related_model = {}
+model = {}
 
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))  
 mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT_NAME"))
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """Initialize the Refactor Model and Embed Model on startup and clean up on shutdown."""
-    chat_model = chat_model_work.RefactorModel()
-    api_related_model['RefactorModel'] = chat_model 
-    contextual_embed_work = contextual_embed.ContextualEmbed()
-    api_related_model['ContextualEmbed'] = contextual_embed_work
+    """Initialize the Refactor model on startup and clean up on shutdown."""
+    model_work = chat_model_work.RefactorModel() # RefactorModel
+    model['RefactorModel'] = model_work 
     yield 
 
-    api_related_model.clear()
+    model.clear()
 
 app = FastAPI(lifespan=lifespan)
 
@@ -29,21 +28,22 @@ app = FastAPI(lifespan=lifespan)
 @mlflow.trace
 def request_text(textRequest: textRequest) -> str:
     """Request Model"""
-    db = api_related_model["ContextualEmbed"]
+    db = RetrieveData()
     db.connect()
-    
-    with mlflow.start_run(run_name="Chatbot Request Run"):
-        mlflow.log_param("input_query", textRequest.SQL_QUERY)
-        try:
-            query =  textRequest.SQL_QUERY
-            questions = db.retrieve_questions()
-            answers = db.retrieve_answers()
-            db.close()
+    db.user_input = textRequest.SQL_QUERY
 
-            final_db = db.concat(questions, answers)
-            response = db.retrieve_embed(query, final_db)
+    with mlflow.start_run(run_name="API_Request_Run"):
+        mlflow.log_param("input_query", textRequest.SQL_QUERY)
+        
+        try:
+
+            ques_ret = db.retrieve_questions()
+            ans_ret = db.retrieve_answers()
+            concat_qa = db.concat(ques_ret, ans_ret)
+            pre_dc = db.preprocessing_doc()
+            take_sim = db.most_sim(concat_qa, pre_dc)
             
-            result_work = api_related_model['RefactorModel'].model_work(response)
+            result_work = model['RefactorModel'].model_work(take_sim)
 
             mlflow.log_param("model_output", result_work)
             return result_work
@@ -51,12 +51,14 @@ def request_text(textRequest: textRequest) -> str:
         except Exception as e:
 
             error_trace = traceback.format_exc()
-            mlflow.log_param("error_type", error_trace)
-            
-            return {"Error Occured"}
-        
-        finally:
-            db.close()
 
+            mlflow.log_param("error_type", error_trace)
+
+            return {"Error Occured"}
+
+        finally:
+
+            db.close()
+            
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=5008, reload=True)
